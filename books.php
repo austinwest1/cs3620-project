9449f92f8e37c279d837d350cef30aff7947b748
austinwest1 <austinwest1@mail.weber.edu>
2021-01-21 18:14:28 -0700

    Added third book to file.

--- books.php
+++ books.php
@@ -2,4 +2,4 @@
 header("Access-Control-Allow-Origin: *")
 ?>
 
-{"books-read":[{"Title":"Iron Gold","Author":"Pierce Brown"},{"Title":"Dark Age", "Author":"Pierce Brown"}]}
+{"books-read":[{"Title":"Iron Gold","Author":"Pierce Brown"},{"Title":"Dark Age", "Author":"Pierce Brown"}, {"Title":"Dark Age", "Author":"Pierce Brown"}]}
