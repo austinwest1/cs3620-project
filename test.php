ac059387380206c2479743de0fdbc44ef25dd6e0
Austin West <austinwest1@mail.weber.edu>
2021-01-27 21:56:22 -0700

    Testing.

--- test.php
+++ test.php
@@ -3,5 +3,3 @@ header("Access-Control-Allow-Origin: *")
 ?>
 
 {"books-read":[{"Title":"Iron Gold","Author":"Pierce Brown"},{"Title":"Dark Age", "Author":"Pierce Brown"}]}
-
-Why isnt this working!!
