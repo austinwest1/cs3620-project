36932bc2f49359d33c94a9941936d9ce67a898e4
Austin West <austinwest1@mail.weber.edu>
2021-01-28 18:35:08 -0700

    testing

--- books.php
+++ books.php
@@ -4,4 +4,4 @@ header("Access-Control-Allow-Origin: *")
 
 {"books-read":[{"Title":"Iron Gold","Author":"Pierce Brown"},{"Title":"Dark Age", "Author":"Pierce Brown"}, {"Title":"Heroes Die", "Author":"Matthew Stover"}]}
 
-test
+test2
