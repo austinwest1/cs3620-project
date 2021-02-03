c7458190e27026bb09e8079691adcfdffaf7bb6c
Austin West <austinwest1@mail.weber.edu>
2021-02-02 17:04:58 -0700

    Formatting

--- book_crud.php
+++ book_crud.php
@@ -40,7 +40,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        echo "id: " . $row["book_id"] . " - Title: " . $row["title"] . " - Author: " . $row["author"] . "<br>";
+        echo "Booki id: " . $row["book_id"] . " - Title: " . $row["title"] . " - Author: " . $row["author"] . "<br>";
     }
 } else {
     echo "0 results";
