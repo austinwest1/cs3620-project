8d481ed50964f47946aefb5d6a1cb3b30944cdfe
Austin West <austinwest1@mail.weber.edu>
2021-02-15 23:18:40 -0700

    Cleaned up code.

--- users.php
+++ users.php
@@ -11,11 +11,6 @@ $un = $_GET["un"];
 $fn = $_GET["fn"];
 $ln = $_GET["ln"];
 
-// echo $id;
-// echo $un;
-// echo $fn;
-// echo $ln;
-
 if ($id != -1) {
     //echo "*";
     $user = new user();
@@ -35,28 +30,3 @@ if ($id != -1) {
 }
 
 echo json_encode($user);
-
-//echo $_GET[1];
-
-// $user = new user();
-// $user->getUser($_GET["id"]);
-
-//$user->getUserByUsername($_GET["un"]);
-
-
-// $user->getUserByFirstname("first_name", $_GET["fn"]);
-// $user->getUserByUsername("username", $_GET["un"]);
-// $user->getUserByLastname("last_name", $_GET["ln"]);
-
-//$fn = $_GET["fn"];
-
-// if ($fn != -1) {
-//     $user->getUserByFirstname("first_name", $_GET["fn"]);
-//     echo "***";
-// } else if ($_GET["un"] != -1) {
-//     $user->getUserByUsername("username", $_GET["un"]);
-// } else if ($_GET["ln"] != -1) {
-//     $user->getUserByLastname("last_name", $_GET["ln"]);
-// } else if ($_GET["id"] != -1) {
-//     $user->getUser($_GET["id"]);
-// }
